45b55ee50a12410a0eddca48866b2d1c6173850a
Dario Roman <darioroman2001@gmail.com>
2021-08-30 09:01:24 -0500

    add more methods.

--- methods.go
+++ methods.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 // Client is an interface to implements all the steam web api methods
@@ -12,9 +13,9 @@ type Client interface {
 	GetGlobalAchievementPercentagesForApp(int)
 	GetPlayerSummaries(int)
 	GetFriendList(int)
-	GetPlayerAchievements()
-	GetUserStatsForGame()
-	GetOwnedGames()
+	GetPlayerAchievements(int int)
+	GetUserStatsForGame(int int)
+	GetOwnedGames(int)
 	GetRecentlyPlayedGames()
 }
 
@@ -29,7 +30,7 @@ type SteamClient struct {
 	baseUrl string // holds the base url of the api
 }
 
-// utility function to
+// utility function to make a get request
 func (s *SteamClient) request(url string) ([]byte, error) {
 	res, err := http.Get(url)
 	if err != nil {
@@ -40,7 +41,7 @@ func (s *SteamClient) request(url string) ([]byte, error) {
 	return ioutil.ReadAll(res.Body)
 }
 
-func (s *SteamClient) GetNewsForApp(appID int, count, maxLen int) (interface{}, error) {
+func (s *SteamClient) GetNewsForApp(appID, count, maxLen int) (interface{}, error) {
 	url := fmt.Sprintf(
 		"%s/ISteamNews/GetNewsForApp/v0002/?appid=%d&count=%d&maxlength=%d&format=json",
 		s.baseUrl, appID, count, maxLen,
@@ -77,6 +78,51 @@ func (s *SteamClient) GetPlayerSummaries(steamID int) (interface{}, error) {
 func (s *SteamClient) GetFriendList(steamID int) (interface{}, error) {
 	url := fmt.Sprintf("%s/GetFriendList/v0001/?key=%s&steamid=%d&relationship=friend", s.baseUrl, s.apiKey, steamID)
 	data, err := makeRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (s *SteamClient) GetPlayerAchievements(steamID, appID int) (interface{}, error) {
+	url := fmt.Sprintf("%s/GetPlayerAchievements/v0001/?appid=%d&key=%s&steamid=%d", s.baseUrl, appID, s.apiKey, steamID)
+	data, err := makeRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (s *SteamClient) GetUserStatsForGame(steamID, appID int) (interface{}, error) {
+	url := fmt.Sprintf("%s/GetUserStatsForGame/v0002/?appid=%d&key=%s&steamid=%d", s.baseUrl, appID, s.apiKey, steamID)
+	data, err := makeRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (s *SteamClient) GetOwnedGames(steamID int) (interface{}, error) {
+	url := fmt.Sprintf("GetOwnedGames/v0001/?key=%s&steamid=%d", s.apiKey, steamID)
+	data, err := makeRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (s *SteamClient) GetRecentlyPlayedGames(steamID int, count ...int) (interface{}, error) {
+	buf := new(strings.Builder)
+	buf.WriteString(fmt.Sprintf("%s/IPlayerService/GetRecentlyPlayedGames/v0001/?key=%s&steamid=%d", s.baseUrl, s.apiKey, steamID))
+	if len(count) != 0 {
+		buf.WriteString(fmt.Sprintf("&count=%d", count[0]))
+	}
+
+	data, err := makeRequest(buf.String())
 	if err != nil {
 		return nil, err
 	}
